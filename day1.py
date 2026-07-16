f2ce3f90c613880298feebdc5e49834ddfb1aa25
AliceMugure <machariamugure17@gmail.com>
2026-07-16 21:04:06 +0300

    initial commit

---
+++ day1.py
@@ -0,0 +1,34 @@
+print("Hello World")
+print("My name Alice Mugure")
+
+print("what is your name ?")
+input
+
+# python data types
+
+# 123 .....integer
+# "I come from Nairobi" .....string
+# 3.12....float
+# true....booolean
+
+# variables
+# They are containers for data storage
+# dont enclose variables in print
+
+# extra spaace matters in python
+# fstring insert avariable inside a string
+# python is case sensitive
+
+
+is_raining = True
+is_hot = False
+
+print(is_raining)
+print(is_hot)
+
+age = 21
+print("I am " , age,"years old")
+# or
+print(f"I am {age} years old")
+# or
+print("I am "  +  str(21)  +  " years old")
---
+++ day2.py
@@ -0,0 +1,1 @@
+print("Today is day 2t")

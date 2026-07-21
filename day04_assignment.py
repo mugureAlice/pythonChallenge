79892dc67a890f6d599e9d0d48e36c7a1bfdb191
AliceMugure <machariamugure17@gmail.com>
2026-07-21 21:33:38 +0300

    Complete Day 4 assignment

--- day04_assignment.py
+++ day04_assignment.py
@@ -2,15 +2,65 @@
 first_name ="Alice"
 last_name = "Mugure"
 age = 20
-height = 56
+height = 1.6764
 course = "cybersecurity"
 university = "Kipchelele"
-yearOfStudy = 2026 
+yearOfStudy = 2
 FavouriteProgramminglanguage = "python"
 DreamCompany = "My company"
 DreamJob = "kujibamba"
 NumberOfSiblings = 50
-oowns_laptop = True
+owns_laptop = True
+has_programming_experince = True
+
+print("FIRSTNAME : ", first_name)   
+print("LASTNAME : ", last_name)  
+print("AGE : ", age) 
+print("HEIGHT : ", height) 
+print("COURSE : ", course) 
+print("UNIVERSITY : ", university) 
+print(f"year of study : {yearOfStudy}" ) 
+print("Favourite Programming language : " + FavouriteProgramminglanguage) 
+print("DreamCompany : " + DreamCompany) 
+print(f"NumberOfSiblings:  {NumberOfSiblings}") 
+print(f"owns_laptop :  {owns_laptop}") 
+print(f"has_programming_experince  :  {has_programming_experince }") 
+
+print(type(first_name))
+print(type(last_name))
+print(type(age))
+print(type(height))
+print(type(course ))
+print(type(university))
+print(type(yearOfStudy))
+print(type(FavouriteProgramminglanguage))
+print(type(DreamCompany))
+print(type(DreamJob))
+print(type(NumberOfSiblings))
+print(type(owns_laptop))
+print(type(has_programming_experince))
+
+#Think Before You Run
+print(type("100")) #string
+print(type(100)) #integer
+print(type(100.5)) #float
+print(type(False)) #boolean
+
+food = "not omena"
+beverage = "coconut water"
+mirror = "colourless"
+is_exercising = False
+weight = 65.5
 
 
+print("food: " + food)   
+print("beverage : " + beverage)  
+print("mirror : " + mirror ) 
+print(f"is_exercising: {is_exercising}") 
+print(f"weight  : { weight } ") 
 
+print(type(food)) 
+print(type( beverage)) 
+print(type(mirror )) 
+print(type(is_exercising))
+print(type(weight ))

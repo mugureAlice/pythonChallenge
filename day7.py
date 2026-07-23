fc58f7a17a2de2890a5d93f9161a07c7798cd284
AliceMugure <machariamugure17@gmail.com>
2026-07-23 20:52:54 +0300

    Day 7 done

---
+++ day7.py
@@ -0,0 +1,75 @@
+#LOOPS
+#Repeat something
+
+print ("Today is Thursday")
+#We want to print this 100 times
+
+#TYPES OF LOOPS
+#While loops
+#For loops
+
+#WHILE LOOPS
+
+# while condition:
+#     statement
+
+# cake = 1
+
+# while cake <= 9 :
+#     print (cake)
+#     cake  += 2
+
+#Infinite loop
+# cake = 1
+# while cake <= 9
+#print(cake)
+     
+
+#FOR LOOPS
+#Repeate over a sequence
+
+# for numbers in range(7):
+#     print(numbers)
+
+
+#RANGE
+#range(5) prints 0-4
+
+# for i in range(5):
+#     print(1)
+
+# for i in range(0,12,4):
+#     print(i)   
+
+# for i in range (1,6):
+#     print(i )    
+
+#LOOPING THROUGH STRINGS
+# for letters in "PYTHON":
+#     print(letters)     
+
+#BREAK
+#Stops the loop immediately
+
+# for i in range (1,11):
+#     if i==6:
+#         break
+#     print(i)
+
+#CONTINUE
+# #Skips one iteration
+
+# for i in range(1,11):
+#     if i==6:
+#         continue
+#     print(i)
+
+#PASSWORD CHECKER
+
+password = "" 
+
+while password != "12345":
+    password = input("Enter your password: ")
+print("WELCOME")
+
+         
